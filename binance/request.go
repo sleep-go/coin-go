91d8fa46e3d58720f392a3a6de02c0a07cde109f
sleep-go <357933909@qq.com>
2024-10-04 10:39:04 +0800

    request.go

--- binance/request.go
+++ binance/request.go
@@ -15,6 +15,8 @@ import (
 	"net/http"
 	"net/url"
 	"reflect"
+
+	"github.com/spf13/cast"
 )
 
 type Request struct {
@@ -33,31 +35,21 @@ func (r *Request) SetNeedSign(needSign bool) *Request {
 	return r
 }
 
-func (r *Request) addParam(key string, value any) *Request {
-	if r.query == nil {
-		r.query = url.Values{}
-	}
-	r.query.Add(key, fmt.Sprintf("%v", value))
-	return r
-}
-
 // SetParam set param with key/value to query string
 func (r *Request) SetParam(key string, value any) *Request {
 	if r.query == nil {
 		r.query = url.Values{}
 	}
-	// 使用反射获取实际的值，避免重复处理不同的指针类型
-	v := reflect.ValueOf(value)
-	if v.Kind() == reflect.Ptr {
-		// 如果是指针并且指向nil，则不设置参数
-		if v.IsNil() {
-			return r
-		}
-		// 获取指针指向的实际值
-		value = v.Elem().Interface()
+	switch value.(type) {
+	case *int, *int8, *int16, *int32, *int64:
+		r.query.Set(key, fmt.Sprintf("%v", cast.ToString(value)))
+	case *uint, *uint8, *uint16, *uint32, *uint64:
+		r.query.Set(key, fmt.Sprintf("%v", cast.ToUint(value)))
+	case *string:
+		r.query.Set(key, fmt.Sprintf("%v", cast.ToString(value)))
+	default:
+		r.query.Set(key, fmt.Sprintf("%v", value))
 	}
-	// 设置查询参数
-	r.query.Set(key, fmt.Sprintf("%v", value))
 	return r
 }
 func (r *Request) SetOptionalParam(key string, value any) *Request {
@@ -67,15 +59,68 @@ func (r *Request) SetOptionalParam(key string, value any) *Request {
 	}
 	// 使用反射获取实际的值
 	v := reflect.ValueOf(value)
-	if v.Kind() == reflect.Ptr {
+	switch v.Kind() {
+	case reflect.Ptr:
 		// 如果是指针并且指向 nil，则不设置参数
 		if v.IsNil() {
 			return r
 		}
 		// 获取指针指向的实际值
 		value = v.Elem().Interface()
+		if value == 0 {
+			return r
+		}
+	case reflect.String:
+		if v.String() == "" {
+			return r
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		if v.Int() == 0 {
+			return r
+		}
+	case reflect.Float32, reflect.Float64:
+		if v.Float() == 0 {
+			return r
+		}
+	default:
+		// 设置查询参数
+		r.query.Set(key, fmt.Sprintf("%v", value))
 	}
+	return r
+}
 
+func (r *Request) SetOptionalParam2(key string, value any) *Request {
+	// 如果 value 为 nil，直接返回
+	if value == nil {
+		return r
+	}
+	// 使用反射获取实际的值
+	v := reflect.ValueOf(value)
+	switch v.Kind() {
+	case reflect.Ptr:
+		if v.IsNil() {
+			return r
+		}
+		// 获取指针指向的实际值
+		value = v.Elem().Interface()
+	case reflect.String:
+		if v.String() == "" {
+			return r
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		if v.Int() == 0 {
+			return r
+		}
+	case reflect.Float32, reflect.Float64:
+		if v.Float() == 0 {
+			return r
+		}
+	case reflect.Bool:
+		if !v.Bool() {
+			return r
+		}
+	default:
+	}
 	// 设置查询参数
 	r.query.Set(key, fmt.Sprintf("%v", value))
 	return r
