5a3818d7004d31b6650d876b2a0cc41fae9afdf3
sleep-go <357933909@qq.com>
2025-04-10 16:02:35 +0800

    tron

--- tron/assets/get_asset_by_id_or_issuer.go
+++ tron/assets/get_asset_by_id_or_issuer.go
@@ -37,7 +37,6 @@ type GetAssetByIdOrIssuerReq struct {
 
 func (a *Assets) GetAssetByIdOrIssuer(req *GetAssetByIdOrIssuerReq) (*GetAssetByIdOrIssuerResp, error) {
 	values := url.Values{}
-	values.Set("identifier", req.Identifier)
 	values.Add("only_confirmed", fmt.Sprintf("%v", req.OnlyConfirmed))
 	path := fmt.Sprintf("/v1/assets/%s", req.Identifier)
 	response, err := a.Client.Get(path, values)
